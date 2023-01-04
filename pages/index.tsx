a1c96e1deba4ef5d1a3b66dbd72504cdb432cf23
semagul <isemagul@gmail.com>
2023-01-05 00:09:59 +0100

    complete header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Reddit 2.0 Clone</title>
+        <title>Reddit 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+    <Header />
     </div>
   )
 }
